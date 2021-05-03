903834adc66ea52a7235734c2af7373020554c15
Arpita <arpya74@gmail.com>
2021-05-02 23:36:17 -0400

    challenge find longest word

---
+++ Longest Word/index.js
@@ -0,0 +1,12 @@
+// Have the function LongestWord(sen) take the sen parameter being passed and return the longest word in the string. If there are two or more words that are the same length, return the first word from the string with that length. Ignore punctuation and assume sen will not be empty. Words may also contain numbers, for example "Hello world123 567"
+
+// Use the Parameter Testing feature in the box below to test your code with different arguments.
+
+function LongestWord(sen) {
+    let words = sen.split(" ").sort(function(a,b) {
+        return b.replace(/[^a-z]/gi,"").length - a.replace(/[^a-z]/gi,"").length;
+    });
+    return words.shift();
+}
+
+LongestWord(readLine());
